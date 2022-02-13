45019f7327ce94f14eaaa390352993d5cecd266f
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2022-02-13 11:41:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function preload(){}
+noseX = 0;
+noseY = 0;
+function preload(){
+    clown_nose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
@@ -10,12 +15,15 @@ function setup(){
 }
 function draw(){
 image(video,0,0,300,300);
+image(clown_nose,noseX,noseY,30,30);
 }
 function gotPoses(results){
     if (results.length > 0){
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x -12 ;
+        noseY = results[0].pose.nose.y -12;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
     }
 }
  function modelLoaded(){
